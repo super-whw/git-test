42726674806a601a1d1462fd7b3b23059dba05a3
Jhon <Jhon@163.com>
2022-03-19 11:11:10 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("hot-fix second commit");
+        System.out.println("master second commit");
     }
 }
